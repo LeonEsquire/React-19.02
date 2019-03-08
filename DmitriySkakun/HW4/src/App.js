83885e4b6068b6cb565b4343962d882ab13a50ad
SkakunD <beenom314@gmail.com>
2019-03-08 12:00:03 +0300

    create HW4 without componentDidMount
    Signed-off-by: SkakunD <beenom314@gmail.com>

--- DmitriySkakun/HW4/src/App.js
+++ DmitriySkakun/HW4/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './Menu';
-import EnterLogin from './Login';
+// import Menu from './Menu';
+// import EnterLogin from './Login';
+import './app/styles/style.css';
+// import DisplayElement from './app/components/DisplayElement'
+import Blog from './app/components/Blog';
+import WelcomeModal from './app/components/WelcomeModal';
+
+
 
 class App extends React.Component {
+    componentDidMount(){
+        console.log(WelcomeModal);
+    };
+
     render(){
-        const menuItem = [
-            {href:"/", title:"Главная"},
-            {href:"/about", title:"О нас"},
-            {href:"/service", title:"Услуги"},
-            {href:"/contacts", title:"Контакты"},
+        const postBlog = [
+            {
+                title: 'About JS',
+                postBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente ducimus tempore libero, minima amet maxime quia nobis sint eos minus soluta tempora aliquid ipsa quis consectetur voluptatem laudantium similique!',
+                author: 'Maks Denissoff'
+            },
+            {
+                title: 'About ReactJS',
+                postBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente ducimus tempore libero, minima amet maxime quia nobis sint eos minus soluta tempora aliquid ipsa quis consectetur voluptatem laudantium similique!',
+                author: 'Lena Webgirl'
+            },
+            {
+                title: 'About Redux',
+                postBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente ducimus tempore libero, minima amet maxime quia nobis sint eos minus soluta tempora aliquid ipsa quis consectetur voluptatem laudantium similique!',
+                author: 'Alan Turing'
+
+            },
+            {
+                title: 'About Developer',
+                postBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente ducimus tempore libero, minima amet maxime quia nobis sint eos minus soluta tempora aliquid ipsa quis consectetur voluptatem laudantium similique!',
+                author: 'Dmitriy Skakun)'
+            }
         ];
 
         return (
-        <>
-            <h1>React</h1>
-            {/* <div>Functional component</div>
-            <Menu titleMenu="Основное меню сайта" items = {menuItem}/> */}
-            <EnterLogin/>
-            <Menu titleMenu = 'Основное меню сайта' items = {menuItem}/>
-        </>
-        )
+            <>
+          <Blog  items = {postBlog}/>
+          </>
+        );
     }
 }
 
