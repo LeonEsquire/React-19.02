7491b02e63129d5c42529f20a026ef36e31aa224
Leon <leon@MacBook-Leon.local>
2019-03-13 09:12:09 +0300

    Lesson7

---
+++ Lesson7/redux-second/src/js/actions/tweetsActions.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export function fetchTweets() {
+  return function(dispatch) {
+    dispatch({type: "FETCH_TWEETS"});
+    
+    axios.get("http://rest.learncode.academy/api/reacttest/tweets")
+      .then((response) => {
+        dispatch({type: "FETCH_TWEETS_FULFILLED", payload: [
+          {id:1, text: 'oh, hi Mark'},
+          {id:2, text: 'you are tearing me apart, Lisa!'}
+          ]
+        })
+      })
+      .catch((err) => {
+        dispatch({type: "FETCH_TWEETS_REJECTED", payload: err})
+      })
+  }
+}
+
+export function addTweet(id, text) {
+  return {
+    type: 'ADD_TWEET',
+    payload: {
+      id,
+      text,
+    },
+  }
+}
+
+export function updateTweet(id, text) {
+  return {
+    type: 'UPDATE_TWEET',
+    payload: {
+      id,
+      text,
+    },
+  }
+}
+
+export function deleteTweet(id) {
+  return { type: 'DELETE_TWEET', payload: id}
+}
